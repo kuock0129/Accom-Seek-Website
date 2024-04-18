64c8382e8c98ce5ee5331e59a4c2ecf1e0509b34
kuock0129 <kuo.ck0129@gmail.com>
2024-04-18 17:52:47 -0500

    frontend update

--- final-project/frontend/README.md
+++ final-project/frontend/README.md
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
+# Accom-Seek
+=======
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+>>>>>>> 65b8365 (Remove nextjs-dashboard as submodule and prepare for commit)
---
+++ final-project/frontend/app/dashboard/page.tsx
@@ -0,0 +1,64 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import '@fontsource/inter';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import DataTable from '@/app/ui/dashboard/DataTable';
+import Filter from '@/app/ui/dashboard/SearchBar';
+import Search from '@/app/ui/search';
+
+
+
+
+export default async function Page() {
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Accom-Seek Dashboard
+      </h1>
+      <Search placeholder="seach hotel" />
+      {/* <h3 className={`${lusitana.className} mb-6 text-m md:text-2xl`}>
+        Location
+      </h3>
+    
+      <Filter />
+      <h3 className={`${lusitana.className} mb-6 text-m md:text-2xl`}>
+        CrimeRate
+      </h3>
+      <Filter />
+      <h3 className={`${lusitana.className} mb-6 text-m md:text-2xl`}>
+        Participition
+      </h3>
+      <Filter />
+      <h3 className={`${lusitana.className} mb-6 text-m md:text-2xl`}>
+        Participition
+      </h3> */}
+
+
+      <div style={{ height: '40px' }} />
+      <DataTable />
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/*頁面是一個非同步元件。這允許您使用await來獲取數據。 */}
+
+
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
+}
